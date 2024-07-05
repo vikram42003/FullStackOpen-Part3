f4c1e573a2323b66572090d0ef6e4f6617373c2e
vikram(wsl) <vikramjit360@gmail.com>
2024-07-05 20:15:29 +0530

    Finish exercise 3.7 - feat: add 'morgan' middleware for logging

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   { 
