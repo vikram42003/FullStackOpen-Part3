78884d60c549157cbc14f759c411b0deadb9820e
vikram(wsl) <vikramjit360@gmail.com>
2024-07-09 23:39:10 +0530

    Finish exercise 3.9 - feat: add cors dependency to allow backend to handle cross-origin requests

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.use(express.json());
 morgan.token("content", (req, res) => Object.keys(req.body).length ? JSON.stringify(req.body) : " ");
