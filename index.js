d7959007dbc957a27fb0e2803a71eea1979c356c
vikram(wsl) <vikramjit360@gmail.com>
2024-07-16 13:10:50 +0530

    3.13 and 3.14 - refactor index.js to use mongoose to fetch data from MongoDB
    - ignore handling duplicate names in POST requests for now
    - DELETE requests and info route has not been implemented as of yet

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
+const Person = require("./models/person");
+
 const PORT = process.env.PORT;
 
 app.use(cors());
@@ -13,97 +15,64 @@ app.use(express.static("dist"));
 morgan.token("content", (req, res) => Object.keys(req.body).length ? JSON.stringify(req.body) : " ");
 app.use("*", morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-function generateId() {
-  let id = (Math.random() * 1000).toFixed(0);
-  if (persons.find(p => p.id === id)) {
-    id = generateId();
-  } else {
-    return id;
-  }
-}
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(result => {
+    res.json(result);
+  })
 })
 
 app.get("/api/persons/:id", (req, res) => {
-  const person = persons.find(p => p.id === req.params.id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id).then(result => {
+    res.json(result);
+  })
 })
 
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons", (req, res) => {
   if (!req.body.name || !req.body.number) {
     return res.status(400).json({ 
       error: 'name and/or number are missing' 
     })
   }
 
-  if (persons.find(p => p.name === req.body.name)) {
-    return res.status(409).json({
-      error: `person named ${req.body.name} already exists in the phonebook. name must be unique`
-    })
-  }
+  // !!! Ignore duplicate names for now
+  // if (persons.find(p => p.name === req.body.name)) {
+  //   return res.status(409).json({
+  //     error: `person named ${req.body.name} already exists in the phonebook. name must be unique`
+  //   })
+  // }
 
-  const newPerson = {
-    "id": generateId(),
-    "name": req.body.name,
-    "number": req.body.number
-  }
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number,
+  })
 
-  persons = persons.concat(newPerson);
-
-  res.json(newPerson);
+  newPerson.save().then(result => {
+    res.json(result);
+  })
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-  const oldLength = persons.length;
+// app.delete("/api/persons/:id", (req, res) => {
+//   const oldLength = persons.length;
 
-  persons = persons.filter(p => p.id !== req.params.id);
+//   persons = persons.filter(p => p.id !== req.params.id);
 
-  if (oldLength !== persons.length) {
-    res.status(204).end();
-  } else {
-    res.statusMessage = `User with id ${req.params.id} does not exist on the server`;
-    res.status(404).end();
-  }
-})
+//   if (oldLength !== persons.length) {
+//     res.status(204).end();
+//   } else {
+//     res.statusMessage = `User with id ${req.params.id} does not exist on the server`;
+//     res.status(404).end();
+//   }
+// })
 
-app.get("/info", (req, res) => {
-  const htmlToSend = (
-    `<div>
-      <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? "person" : "people"}</p>
-      <p>${new Date().toString()}</p>
-    </div>`
-  )
-  res.send(htmlToSend);
-})
+// app.get("/info", (req, res) => {
+//   const htmlToSend = (
+//     `<div>
+//       <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? "person" : "people"}</p>
+//       <p>${new Date().toString()}</p>
+//     </div>`
+//   )
+//   res.send(htmlToSend);
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is live on port ${PORT}`);
