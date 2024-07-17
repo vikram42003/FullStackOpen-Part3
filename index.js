ce22a4d01d0a511563c919600c37b00b81092b7d
vikram(wsl) <vikramjit360@gmail.com>
2024-07-17 13:31:15 +0530

    3.15 - feat: add handling for DELETE requests

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-morgan.token("content", (req, res) => Object.keys(req.body).length ? JSON.stringify(req.body) : " ");
-app.use("*", morgan(":method :url :status :res[content-length] - :response-time ms :content"))
+morgan.token("content", (req, res) => (Object.keys(req.body).length ? JSON.stringify(req.body) : " "));
+app.use("*", morgan(":method :url :status :res[content-length] - :response-time ms :content"));
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then(result => {
     res.json(result);
-  })
-})
+  });
+});
 
 app.get("/api/persons/:id", (req, res) => {
   Person.findById(req.params.id).then(result => {
     res.json(result);
-  })
-})
+  });
+});
 
 app.post("/api/persons", (req, res) => {
   if (!req.body.name || !req.body.number) {
-    return res.status(400).json({ 
-      error: 'name and/or number are missing' 
-    })
+    return res.status(400).json({
+      error: "name and/or number are missing",
+    });
   }
 
   // !!! Ignore duplicate names for now
@@ -44,25 +44,20 @@ app.post("/api/persons", (req, res) => {
   const newPerson = new Person({
     name: req.body.name,
     number: req.body.number,
-  })
+  });
 
   newPerson.save().then(result => {
     res.json(result);
-  })
-})
+  });
+});
 
-// app.delete("/api/persons/:id", (req, res) => {
-//   const oldLength = persons.length;
-
-//   persons = persons.filter(p => p.id !== req.params.id);
-
-//   if (oldLength !== persons.length) {
-//     res.status(204).end();
-//   } else {
-//     res.statusMessage = `User with id ${req.params.id} does not exist on the server`;
-//     res.status(404).end();
-//   }
-// })
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(error => next(error));
+});
 
 // app.get("/info", (req, res) => {
 //   const htmlToSend = (
@@ -76,4 +71,4 @@ app.post("/api/persons", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is live on port ${PORT}`);
-})
+});
